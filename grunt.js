31b866c0f297d073af0d464524859e254473eab5
Davi Ferreira <davi.ferreira@corp.globo.com>
2012-10-26 16:45:09 -0200

    4 spaces

--- grunt.js
+++ grunt.js
@@ -1,67 +1,66 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: '<json:jquery.stickysidebar.json>',
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-    },
-    min: {
-      js: {
-        src: 'src/jquery.<%= pkg.name %>.js',
-        dest: 'dist/jquery.<%= pkg.name %>.min.js'
-      }
-    },
-    cssmin: {
-      css: {
-        src: 'src/jquery.<%= pkg.name %>.css',
-        dest: 'dist/jquery.<%= pkg.name %>.min.css'
-      }
-    },
-    csslint: {
-      base: {
-        src: "src/*.css",
-        rules: {
-          "import": false,
-          "overqualified-elements": 2
-        }
-      }
-    },
-    lint: {
-      files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint'
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true
-      },
-      globals: {
-        jQuery: true
-      }
-    },
-    uglify: {}
-  });
+    grunt.initConfig({
+        pkg: '<json:jquery.stickysidebar.json>',
+        meta: {
+            banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+                '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+                '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+                ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+        },
+        min: {
+            js: {
+                src: 'src/jquery.<%= pkg.name %>.js',
+                dest: 'dist/jquery.<%= pkg.name %>.min.js'
+            }
+        },
+        cssmin: {
+            css: {
+                src: 'src/jquery.<%= pkg.name %>.css',
+                dest: 'dist/jquery.<%= pkg.name %>.min.css'
+            }
+        },
+        csslint: {
+            base: {
+                src: "src/*.css",
+                rules: {
+                    "import": false,
+                    "overqualified-elements": 2
+                }
+            }
+        },
+        lint: {
+            files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
+        },
+        watch: {
+            files: '<config:lint.files>',
+            tasks: 'lint'
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                browser: true
+            },
+            globals: {
+                jQuery: true
+            }
+        },
+        uglify: {}
+    });
 
-  grunt.loadNpmTasks('grunt-css');
+    grunt.loadNpmTasks('grunt-css');
 
-  grunt.registerTask('default', 'lint min cssmin csslint');
+    grunt.registerTask('default', 'lint min cssmin csslint');
 
 };
