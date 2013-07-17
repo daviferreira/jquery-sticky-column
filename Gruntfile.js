a55be696ba747b7e1da0bb6ef11587cd855cbb8c
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-07-17 09:56:22 -0300

    Generates new dist versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        pkg: '<json:package.json>',
+        pkg: grunt.file.readJSON('package.json'),
         meta: {
             banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
                 '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
